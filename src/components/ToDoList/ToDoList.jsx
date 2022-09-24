1ecad09124658543c864bb1c309ef5ad6be8dc94
wilbernp <wilbernarvaezpetro@gmail.com>
2022-09-24 10:21:28 -0500

    agregando drag and drop

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import { ReactSortable } from "react-sortablejs";
 import { useDispatch, useSelector } from 'react-redux'
-import { clearTodosCompleted, deleteTodo, handleActiveCheckbox, handleComplete } from '../../redux/slices/todosSlice'
+import { clearTodosCompleted, deleteTodo, handleActiveCheckbox, handleComplete, setTodos } from '../../redux/slices/todosSlice'
 import Checkbox from '../Checkbox/Checkbox'
 import "./toDoList.css"
 import cross_icon from "../../assets/images/icon-cross.svg"
@@ -26,37 +27,57 @@ export default function ToDoList() {
     return (
         <div className={container_todo_list}>
             {
-                filterTodos.length ? filterTodos.map((toDo) => {
-                    return (
-                        <div
-                            className={`todo-item ${lightMode && "todo-item-light"}`}
-                            key={toDo.id}>
-                            <Checkbox 
-                                handleActive={(bool) =>handleActive(bool, toDo.id)}
-                                active={toDo.complete} 
-                                lightMode={lightMode}/>
-                            <label
-                                className={`label-checkbox 
+                !filterTodos.length ?
+                    <h2 className='not-todo-text'>{message}</h2> :
+                    <>
+                    <ReactSortable
+                    list={filterTodos.map(todo =>{
+                        return{
+                            id: todo.id,
+                            title: todo.title,
+                            complete: todo.complete
+                          }
+                    })}
+
+                    setList={(list) => dispatch(setTodos(list))}
+
+                    dragClass="drag"
+
+                    animation={400}
+                    >
+                        {filterTodos.map((toDo) => {
+                            return (
+                                <div
+                                    className={`todo-item ${lightMode && "todo-item-light"}`}
+                                    key={toDo.id}>
+                                    <Checkbox
+                                        handleActive={(bool) => handleActive(bool, toDo.id)}
+                                        active={toDo.complete}
+                                        lightMode={lightMode} />
+                                    <label
+                                        className={`label-checkbox 
                                     ${toDo.complete &&
-                                    "label-checkbox-complete"}`}
-                            >
-                                {toDo.title}
-                            </label>
-                            <a className='button-delete-todo'
-                                onClick={() => handleDelete(toDo.id)}>
-                                <img src={cross_icon} alt="cross-icon" />
-                            </a>
+                                            "label-checkbox-complete"}`}
+                                    >
+                                        {toDo.title}
+                                    </label>
+                                    <a className='button-delete-todo'
+                                        onClick={() => handleDelete(toDo.id)}>
+                                        <img src={cross_icon} alt="cross-icon" />
+                                    </a>
 
+                                </div>
+                            )
+                        })}
+                    </ReactSortable>
+
+
+                        <div className='footer-todo-list'>
+                            <span className='info-footer-todo'>{items_left.length} items left</span>
+                            <a className='info-footer-todo' onClick={() => dispatch(clearTodosCompleted())}>Clear Completed</a>
                         </div>
-                    )
-                }) : <h2 className='not-todo-text'>{message}</h2>
+                    </>
 
-            }
-
-            {filterTodos.length ? <div className='footer-todo-list'>
-                <span className='info-footer-todo'>{items_left.length} items left</span>
-                <a className='info-footer-todo' onClick={() => dispatch(clearTodosCompleted())}>Clear Completed</a>
-            </div> : null
             }
         </div>
     )
